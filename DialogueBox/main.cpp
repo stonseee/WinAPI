2b94e2ae0e51710abd66ccb312e07968a0970e4f
stonseee <stonseee@STONSEEE>
2024-10-29 20:58:37 +0300

    Copy DONE

--- DialogueBox/main.cpp
+++ DialogueBox/main.cpp
@@ -22,6 +22,18 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_COPY:
+		{
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			//SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			//SendMessage(GetDlgItem(hwnd, IDC_BUTTON_COPY), WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+			break;
 		case IDOK: MessageBox(hwnd, "Была нажата кнопка окей!!", "Info", MB_OK | MB_ICONINFORMATION); 
 			break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;

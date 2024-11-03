b744eeb89cde5404630ba91cb9ea3507379b0fee
stonseee <stonseee@STONSEEE>
2024-11-03 15:56:07 +0300

    Homework DONE

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -8,8 +8,8 @@ CONST CHAR* g_VALUES[] = { "This", "is", "my", "first", "Combo", "Dick" };
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
-{
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+{	
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);	
 	return 0;
 }
 
@@ -18,7 +18,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
-	{
+	{		
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 		HWND hList = GetDlgItem(hwnd, IDC_LIST);
@@ -26,7 +26,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)g_VALUES[i]);
 		}
-		SendMessage(hList, LB_SETCURSEL, 0, 0);
+		SendMessage(hList, LB_SETCURSEL, 0, 0);		
 	}
 	break;
 	case WM_COMMAND:
@@ -46,8 +46,16 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		break;
 		case IDC_BUTTON_ADD:
 		{			
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE];
 			HWND hList = GetDlgItem(hwnd, IDC_LIST);
-			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)"Pic");
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);			
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (strcmp(sz_buffer, "") != 0)
+			{
+				SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			}			
 		}
 		break;
 		case IDC_BUTTON_DELETE:
@@ -58,6 +66,23 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			INT i = SendMessage(hList, LB_GETCURSEL, 0, 0);
 			SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
 			SendMessage(hList, LB_DELETESTRING, i, (LPARAM)sz_buffer);
+		}
+		break;
+		case IDC_LIST:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE];
+			HWND hList = GetDlgItem(hwnd, IDC_LIST);
+			INT i = SendMessage(hList, LB_GETCURSEL, 0, 0);
+			SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+
+			if (HIWORD(wParam) == LBN_DBLCLK && strcmp(sz_buffer, "") != 0)
+			{					
+				SendMessage(hList, LB_INSERTSTRING, i + 1, (LPARAM)sz_buffer);
+				SendMessage(hList, LB_DELETESTRING, i, (LPARAM)sz_buffer);
+			}
 		}
 		break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
--- ListBox/resource.h
+++ ListBox/resource.h
@@ -6,16 +6,17 @@
 #define IDI_ICON1                       105
 #define IDC_LIST                        1001
 #define IDC_BUTTON_ADD                  1002
-#define IDC_BUTTON                      1003
 #define IDC_BUTTON_DELETE               1003
+#define IDC_EDIT1                       1005
+#define IDC_EDIT                        1005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

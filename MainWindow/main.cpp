e7654e9c9a28f2ba73c11b3a306aa0a89b7cf5d3
stonseee <stonseee@STONSEEE>
2024-11-07 21:28:09 +0300

    MainWindow started

---
+++ MainWindow/main.cpp
@@ -0,0 +1,76 @@
+#include<Windows.h>
+
+CONST CHAR g_sz_MY_WINDOW_CLASS[] = "My Window";
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(wc));
+
+	wc.style = 0;
+	wc.cbSize = sizeof(wc);
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+
+	wc.hInstance = hInstance;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = g_sz_MY_WINDOW_CLASS;
+	wc.lpfnWndProc = (WNDPROC)WndProc;
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Class registration failed", NULL, MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	HWND hwnd = CreateWindowEx
+	(
+		NULL,                           //ExStyles
+		g_sz_MY_WINDOW_CLASS,           //Class name
+		g_sz_MY_WINDOW_CLASS,           //Window title
+		WS_OVERLAPPEDWINDOW,			//Window style
+		CW_USEDEFAULT, CW_USEDEFAULT,   //Window position
+		CW_USEDEFAULT, CW_USEDEFAULT,	//Window size
+		NULL,							//Parent Window
+		NULL,							//Main menu ResourceID for MainWindow or ResourceID for ChildWindow
+		hInstance, 
+		NULL
+	);
+	ShowWindow(hwnd, nCmdShow); //задает режим отображения окна
+	UpdateWindow(hwnd);			//прорисовывает окно
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.message;
+}
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	default:		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return FALSE;
+}

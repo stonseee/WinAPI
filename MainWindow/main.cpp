0caf56ac305c97e4d6e7175565bf2e0ae921db19
stonseee <stonseee@STONSEEE>
2024-11-09 20:17:29 +0300

    Window SIze & Position DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,4 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<cstdio>
+#include<iostream>
 
 CONST CHAR g_sz_MY_WINDOW_CLASS[] = "My Window";
 
@@ -30,14 +33,23 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		return 0;
 	}
 
+	INT screen_width = GetSystemMetrics(SM_CXSCREEN);
+	INT screen_height = GetSystemMetrics(SM_CYSCREEN);
+
+	INT window_width = screen_width * 3 / 4;
+	INT window_height = screen_height * .75;
+
+	INT window_start_x = screen_width / 8;
+	INT window_start_y = screen_height / 8;
+
 	HWND hwnd = CreateWindowEx
 	(
 		NULL,                           //ExStyles
 		g_sz_MY_WINDOW_CLASS,           //Class name
 		g_sz_MY_WINDOW_CLASS,           //Window title
 		WS_OVERLAPPEDWINDOW,			//Window style
-		CW_USEDEFAULT, CW_USEDEFAULT,   //Window position
-		CW_USEDEFAULT, CW_USEDEFAULT,	//Window size
+		window_start_x, window_start_y,   //Window position
+		window_width, window_height,	//Window size
 		NULL,							//Parent Window
 		NULL,							//Main menu ResourceID for MainWindow or ResourceID for ChildWindow
 		hInstance, 
@@ -51,6 +63,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
+		
 	}
 
 	return msg.message;
@@ -61,6 +74,30 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_CREATE:
+		AllocConsole();
+		freopen("CONOUT$", "w", stdout);
+		break;
+	case WM_MOVE:
+	case WM_SIZE:
+	{
+		RECT rect;
+		GetWindowRect(hwnd, &rect);
+		INT window_width = rect.right - rect.left;
+		INT window_height = rect.bottom - rect.top;
+		CONST INT SIZE = 256;
+		CHAR sz_title[SIZE]{};
+		sprintf(
+			sz_title, 
+			"%s - Position: %ix%i;\t\tSize: %ix%i", 
+			g_sz_MY_WINDOW_CLASS, 
+			rect.left,
+			rect.top,
+			window_width,
+			window_height
+		);
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_title);
+		//printf()
+	}
 		break;
 	case WM_COMMAND:
 		break;

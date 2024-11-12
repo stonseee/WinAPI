c0eb3adfe1aeb9632d570c3e6caf78d3497e7a14
stonseee <stonseee@STONSEEE>
2024-11-12 21:36:35 +0300

    TABSTOP DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -91,9 +91,9 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0) > 0)
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-		
+		//TranslateMessage(&msg);
+		//DispatchMessage(&msg);		
+		IsDialogMessage(hwnd, &msg);
 	}
 
 	return msg.message;
@@ -106,6 +106,45 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CREATE:
 		//AllocConsole();
 		freopen("CONOUT$", "w", stdout);
+		CreateWindowEx
+		(
+			NULL,
+			"Static",
+			"Здесь могла быть наша реклама",
+			WS_CHILD | WS_VISIBLE,
+			10, 10,
+			500, 25,
+			hwnd,
+			(HMENU)IDC_STATIC, 
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL,
+			"Edit",
+			"",
+			WS_CHILDWINDOW | WS_VISIBLE | WS_BORDER | WS_TABSTOP | WS_GROUP,
+			10, 48,
+			415, 20,
+			hwnd,
+			(HMENU)IDC_EDIT,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL,
+			"Button",
+			"Принемить",
+			WS_CHILD | WS_VISIBLE | WS_TABSTOP | WS_GROUP,
+			275, 85,
+			150, 32,
+			hwnd,
+			(HMENU)IDC_BUTTON,
+			GetModuleHandle(NULL),
+			NULL
+		);
 		break;
 	case WM_MOVE:
 	case WM_SIZE:
@@ -130,6 +169,20 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 		break;
 	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON:
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			HWND hStatic = GetDlgItem(hwnd, IDC_STATIC);
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hStatic, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SendMessage(GetDlgItem(hwnd, IDC_BUTTON), WM_SETTEXT, 0, (LPARAM)sz_buffer);
+
+			break;
+		}
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
--- MainWindow/resource.h
+++ MainWindow/resource.h
@@ -5,6 +5,9 @@
 #define IDC_CURSOR1                     101
 #define IDI_ICON1                       103
 #define IDI_ICON2                       104
+#define IDC_STATIC						1000
+#define IDC_EDIT						1001
+#define IDC_BUTTON						1002
 
 // Next default values for new objects
 // 
